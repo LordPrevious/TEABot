d9efa556af41ec4d59740b48745d45a2ecae2a66
LordPrevious <71894789+LordPrevious@users.noreply.github.com>
2021-04-22 20:30:31 +0200

    Unit test project added, moved solution file
    Solution file moved up a level for better project directory structure now what there's the unit test project.

---
+++ TEABotTests/IRC/TIrcMessageTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TEABot.IRC;
+
+namespace TEABotTests.IRC
+{
+    [TestClass]
+    public class TIrcMessageTests
+    {
+        [TestMethod]
+        public void MessageTags_Parse_SingleTag()
+        {
+            // arrange test data
+            var rawTags = @"tagName=tagValue";
+
+            // call method under test
+            var parsedTags = TIrcMessage.MessageTags.Parse(rawTags);
+
+            // assert method effects
+            // TODO Assert.AreEqual(1, parsedTags.Tags.Count, "Incorrect number of tags parsed");
+        }
+    }
+}
