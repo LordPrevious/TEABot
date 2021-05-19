d0b7cb9a7f881f4812118e8eb6c3d6cb40e812b6
LordPrevious <71894789+LordPrevious@users.noreply.github.com>
2021-05-19 14:16:41 +0200

    Emote list parsing
    I need these the most.

---
+++ TEABot/Twitch/TTwEmotes.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEABot.Twitch
+{
+    /// <summary>
+    /// Message emote information.
+    /// An emote has an ID which is required to find its image on Twitch
+    /// and a name by which it is embedded in a message.
+    /// </summary>
+    class TTwEmotes
+    {
+        #region Constructors
+
+        /// <summary>
+        /// initialize from a list of emotes
+        /// </summary>
+        /// <param name="a_emotes">The emotes</param>
+        public TTwEmotes(IEnumerable<Emote> a_emotes)
+        {
+            mEmotes.AddRange(a_emotes);
+        }
+
+        /// <summary>
+        /// Parse twitch tags to extract emote information.
+        /// </summary>
+        /// <param name="a_emotes">The "emotes" tag value</param>
+        /// <param name="a_message">The chat message to extract emote names from</param>
+        public TTwEmotes(string a_emotes, string a_message)
+        {
+            // allow null arguments
+            var emoteList = a_emotes ?? String.Empty;
+            var message = a_message ?? String.Empty;
+
+            // parse emote list
+            var splitEmoteList = emoteList.Split(cAEmoteListSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (var emoteItem in splitEmoteList)
+            {
+                // split "id:position"
+                var splitEmoteItem = emoteItem.Split(cPositionSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                if (splitEmoteItem.Length >= 2)
+                {
+                    var emoteId = splitEmoteItem[0];
+                    // split position list
+                    var splitPositionList = splitEmoteItem[1].Split(cPositionListSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                    foreach (var positionItem in splitPositionList)
+                    {
+                        // split "start-end"
+                        var splitPositionItem = positionItem.Split(cARangeSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                        if ((splitPositionItem.Length == 2)
+                            && int.TryParse(splitPositionItem[0], out int rangeStart)
+                            && int.TryParse(splitPositionItem[1], out int rangeEnd))
+                        {
+                            // ensure ranges are within the message string and range < rangeEnd
+                            var emoteName = String.Empty;
+                            if (String.IsNullOrEmpty(message))
+                            {
+                                rangeStart = 0;
+                                rangeEnd = 0;
+                            }
+                            else
+                            {
+                                rangeEnd = (rangeEnd < 0) ? 0 : Math.Min(rangeEnd, message.Length - 1);
+                                rangeStart = (rangeStart < 0) ? 0 : Math.Min(rangeStart, rangeEnd);
+                                // extract name from message
+                                emoteName = message[rangeStart..(rangeEnd + 1)];
+                            }
+
+                            // add to emote list
+                            mEmotes.Add(new Emote(emoteId, emoteName, new Range(rangeStart, rangeEnd)));
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public data
+
+        /// <summary>
+        /// List of all the emotes specified by the source tags
+        /// </summary>
+        public IReadOnlyList<Emote> Emotes { get { return mEmotes; } }
+        private readonly List<Emote> mEmotes = new();
+
+        #endregion
+
+        #region Internal data
+
+        private static readonly char cEmoteListSeparator = '/';
+        private static readonly char[] cAEmoteListSeparator = new[] { cEmoteListSeparator };
+        private static readonly char cPositionSeparator = ':';
+        private static readonly char[] cAPositionSeparator = new[] { cPositionSeparator };
+        private static readonly char cPositionListSeparator = ',';
+        private static readonly char[] cAPositionListSeparator = new[] { cPositionListSeparator };
+        private static readonly char cRangeSeparator = '-';
+        private static readonly char[] cARangeSeparator = new[] { cRangeSeparator };
+
+        #endregion
+
+        #region Internal structures
+
+        public readonly struct Emote
+        {
+            /// <summary>
+            /// Emote ID via which its images can be accessed on Twitch
+            /// </summary>
+            public readonly string Id;
+            /// <summary>
+            /// Emote name via which it is embedded in the message
+            /// </summary>
+            public readonly string Name;
+            /// <summary>
+            /// Position of the emote occurrence within the chat message
+            /// </summary>
+            public readonly Range Position;
+
+            /// <summary>
+            /// Initialize an emote struct
+            /// </summary>
+            /// <param name="a_name">Badge name</param>
+            /// <param name="a_version">Version</param>
+            /// <param name="a_info">Info</param>
+            public Emote(string a_id, string a_name, Range a_position)
+            {
+                Id = a_id ?? throw new ArgumentNullException(nameof(a_id));
+                Name = a_name ?? throw new ArgumentNullException(nameof(a_name)); ;
+                Position = a_position;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TEABotTests/Twitch/TTwEmotesTests.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TEABot.Twitch;
+
+namespace TEABotTests.Twitch
+{
+    [TestClass]
+    public class TTwEmotesTests
+    {
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_Null()
+        {
+            TTwEmotes parsedEmotes = new(null, null);
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(0, parsedEmotes.Emotes.Count);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_SingleEmote()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:0-11",
+                "lordpr2Hello");
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(1, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr2Hello", emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(11, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_SingleEmote_NoMessage()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:0-11",
+                null);
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(1, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            // No name could be extracted
+            Assert.AreEqual(String.Empty, emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            // Position should be adjusted so it can be safely used to access a source message later
+            // without requiring checking the message bounds
+            Assert.AreEqual(0, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_SingleEmote_EmptyMessage()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:0-11",
+                String.Empty);
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(1, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual(string.Empty, emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(0, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_MultiEmotes()
+        {
+            TTwEmotes parsedEmotes = new(
+                "307683255:28-44/306981865:0-11",
+                "lordpr2Hello frens this has gamepa13Mildpanic 'motes");
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(2, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("307683255", emote.Id);
+            Assert.AreEqual("gamepa13Mildpanic", emote.Name);
+            Assert.AreEqual(28, emote.Position.Start);
+            Assert.AreEqual(44, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[1];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr2Hello", emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(11, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_DuplicateEmotes()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:11-22,24-35,49-60/emotesv2_b7c104b7df764573b503257a8965631f:37-47,62-72/307070118:0-9",
+                "gamepa13Hi lordpr2Hello lordpr3Hello lordpr2Coco lordpr4Hello lordpr2Cofe");
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(6, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr2Hello", emote.Name);
+            Assert.AreEqual(11, emote.Position.Start);
+            Assert.AreEqual(22, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[1];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr3Hello", emote.Name);
+            Assert.AreEqual(24, emote.Position.Start);
+            Assert.AreEqual(35, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[2];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr4Hello", emote.Name);
+            Assert.AreEqual(49, emote.Position.Start);
+            Assert.AreEqual(60, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[3];
+            Assert.AreEqual("emotesv2_b7c104b7df764573b503257a8965631f", emote.Id);
+            Assert.AreEqual("lordpr2Coco", emote.Name);
+            Assert.AreEqual(37, emote.Position.Start);
+            Assert.AreEqual(47, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[4];
+            Assert.AreEqual("emotesv2_b7c104b7df764573b503257a8965631f", emote.Id);
+            Assert.AreEqual("lordpr2Cofe", emote.Name);
+            Assert.AreEqual(62, emote.Position.Start);
+            Assert.AreEqual(72, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[5];
+            Assert.AreEqual("307070118", emote.Id);
+            Assert.AreEqual("gamepa13Hi", emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(9, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_Overlap()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:11-22/emotesv2_b7c104b7df764573b503257a8965631f:24-34,15-29/307070118:0-9",
+                "gamepa13Hi lordpr2Hello lordpr2Coco");
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(4, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr2Hello", emote.Name);
+            Assert.AreEqual(11, emote.Position.Start);
+            Assert.AreEqual(22, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[1];
+            Assert.AreEqual("emotesv2_b7c104b7df764573b503257a8965631f", emote.Id);
+            Assert.AreEqual("lordpr2Coco", emote.Name);
+            Assert.AreEqual(24, emote.Position.Start);
+            Assert.AreEqual(34, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[2];
+            Assert.AreEqual("emotesv2_b7c104b7df764573b503257a8965631f", emote.Id);
+            Assert.AreEqual("pr2Hello lordpr", emote.Name);
+            Assert.AreEqual(15, emote.Position.Start);
+            Assert.AreEqual(29, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[3];
+            Assert.AreEqual("307070118", emote.Id);
+            Assert.AreEqual("gamepa13Hi", emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(9, emote.Position.End);
+        }
+
+        [TestMethod]
+        public void TTwEmotes_Construct_Parse_OutOfBounds()
+        {
+            TTwEmotes parsedEmotes = new(
+                "306981865:11-22/emotesv2_b7c104b7df764573b503257a8965631f:24-34/307070118:0-9",
+                "gamepa13Hi lordpr2Hello lordpr2C");
+
+            Assert.IsNotNull(parsedEmotes.Emotes);
+            Assert.AreEqual(3, parsedEmotes.Emotes.Count);
+
+            var emote = parsedEmotes.Emotes[0];
+            Assert.AreEqual("306981865", emote.Id);
+            Assert.AreEqual("lordpr2Hello", emote.Name);
+            Assert.AreEqual(11, emote.Position.Start);
+            Assert.AreEqual(22, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[1];
+            Assert.AreEqual("emotesv2_b7c104b7df764573b503257a8965631f", emote.Id);
+            Assert.AreEqual("lordpr2C", emote.Name);
+            Assert.AreEqual(24, emote.Position.Start);
+            Assert.AreEqual(31, emote.Position.End);
+
+            emote = parsedEmotes.Emotes[2];
+            Assert.AreEqual("307070118", emote.Id);
+            Assert.AreEqual("gamepa13Hi", emote.Name);
+            Assert.AreEqual(0, emote.Position.Start);
+            Assert.AreEqual(9, emote.Position.End);
+        }
+    }
+}
