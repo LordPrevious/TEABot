57d7cbe98501d53a9e66928d5f441a448dc53140
LordPrevious <71894789+LordPrevious@users.noreply.github.com>
2021-05-19 19:07:18 +0200

    New list commands: limit, shuffle, sort

---
+++ TEABot/TEAScript/Instructions/Statements/List/TSISListShuffle.cs
@@ -0,0 +1,24 @@
+﻿using TEABot.TEAScript.Attributes;
+
+namespace TEABot.TEAScript.Instructions.Statements.List
+{
+    /// <summary>
+    /// Shuffle the list items
+    /// </summary>
+    [TSKeyword("list:shuffle")]
+    public class TSISListShuffle : TSISNoArguments
+    {
+        public override ITSControlFlow Execute(TSExecutionContext a_context)
+        {
+            if (a_context.Lists == null)
+            {
+                a_context.Broadcaster.Error("A list manager is required.");
+            }
+            else
+            {
+                a_context.Lists.ShuffleListItems();
+            }
+            return TSFlow.Next;
+        }
+    }
+}
---
+++ TEABot/TEAScript/Instructions/Statements/List/TSISListSort.cs
@@ -0,0 +1,34 @@
+﻿using TEABot.TEAScript.Attributes;
+
+namespace TEABot.TEAScript.Instructions.Statements.List
+{
+    /// <summary>
+    /// Sort the list by the given key
+    /// </summary>
+    [TSKeyword("list:sort")]
+    public class TSISListSort : TSIStatement
+    {
+        /// <summary>
+        /// Key to order by
+        /// </summary>
+        private ITSValueArgument mKeyName = new TSConstantStringArgument();
+
+        public override ITSControlFlow Execute(TSExecutionContext a_context)
+        {
+            if (a_context.Lists == null)
+            {
+                a_context.Broadcaster.Error("A list manager is required.");
+            }
+            else
+            {
+                a_context.Lists.SortListItems(mKeyName.GetValue(a_context.Values));
+            }
+            return TSFlow.Next;
+        }
+
+        protected override bool Parse(string a_instructionArguments)
+        {
+            return SingleValueArgument(a_instructionArguments, out mKeyName);
+        }
+    }
+}
